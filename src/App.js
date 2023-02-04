0239fd72316910b770bdbb8f7ea3a336621ce092
Outkarm <jkpordje@gmail.com>
2023-02-04 22:43:52 +0000

    final fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { Route, Routes, Link } from 'react-router-dom';
-import React from 'react';
-
-import Categories from './components/Status';
-import './styles/app.css';
-import BookStore from './components/BookStore';
+/* eslint-disable */
+import { Route, Routes, Link } from "react-router-dom";
+import React from "react";
+import Categories from "./components/Status";
+import "./styles/app.css";
+import BookStore from "./components/BookStore";
 
 function App() {
   return (
@@ -12,13 +12,13 @@ function App() {
         <h1>Bookstore CMS</h1>
         <ul className="nav-links">
           <li className="link">
-            {' '}
+            {" "}
             <Link className="Books" to="/">
               Books
             </Link>
           </li>
           <li className="linked">
-            {' '}
+            {" "}
             <Link className="category" to="/Categories">
               Category
             </Link>
